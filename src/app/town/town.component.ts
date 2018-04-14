50942f7d8666684b36a28be02c1fbcc09715f206
Thibaud REY <contact@thibaudrey.pro>
2018-04-14 20:46:31 +0200

    refactore town component

--- src/app/town/town.component.ts
+++ src/app/town/town.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Town} from "../town";
 
 @Component({
   selector: 'app-town',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./town.component.css']
 })
 export class TownComponent implements OnInit {
+
+  @Input() town: Town;
 
   constructor() { }
 
